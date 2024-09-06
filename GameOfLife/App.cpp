ccd7de998f91ac73e39ff561d290e69ef01610c2
Ceaser Hernandez <ceaserh@live.com>
2024-09-06 13:32:33 -0700

    Created MainWindow class that inherits from wxFrame, Defined constructor to pass args for wxFrame and created empty destructor, declared MainWindow in App.h, instantiated MainWindow pointer in OnInIt, created a main.cpp for simpler launcher and moved wxIMPLEMENT_APP(App) to it.

---
+++ GameOfLife/App.cpp
@@ -0,0 +1,19 @@
+#include "App.h"
+
+
+
+App::App() {
+
+}
+
+App::~App() {
+
+}
+
+bool App::OnInit() {
+
+	mainWindow = new MainWindow();
+	mainWindow->Show();
+
+	return true;
+}
---
+++ GameOfLife/MainWindow.cpp
@@ -0,0 +1,5 @@
+#include "MainWindow.h"
+
+MainWindow::MainWindow() : wxFrame(nullptr, wxID_ANY, "Game of Life", wxPoint(0, 0), wxSize(200, 200)) {}
+
+MainWindow::~MainWindow(){}
